10942fb20d214987e5990ea1117098049270d193
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-01-20 00:05:22 +0300

    feat: update table page title

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [CommonModule, MatCheckboxModule],
-  exports: [MatCheckboxModule],
+  imports: [CommonModule, MatCheckboxModule, MatToolbarModule],
+  exports: [MatCheckboxModule, MatToolbarModule],
 })
 export class MaterialModule {}
