d43090c395ae8789b1469266883693b13835dc24
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-02-02 22:24:46 +0300

    refactor: describe youtube API data

--- src/app/shared/interface/table.interface.ts
+++ src/app/shared/interface/table.interface.ts
@@ -16,3 +16,30 @@ export interface ITableRowData {
   description: string;
   videoId: string;
 }
+
+/**
+ * Youtube API data
+ */
+export interface IResponseTableDataItem {
+  [key: string]: any;
+  snippet: {
+    [key: string]: any;
+    thumbnails: {
+      default: ITableRowData['thumbnail'];
+      medium: ITableRowData['thumbnail'];
+      high: ITableRowData['thumbnail'];
+    };
+    publishedAt: ITableRowData['publishedAt'];
+    title: string;
+    description: string;
+  };
+  id: {
+    kind: string;
+    videoId: string;
+  };
+}
+
+export interface IResponseTableData {
+  [key: string]: any;
+  items: IResponseTableDataItem[];
+}
