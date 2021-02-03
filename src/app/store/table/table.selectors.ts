547920156da0ebc88656e3a2c30289ecd74f7c64
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-02-03 08:10:34 +0300

    feat: add selector tests

--- src/app/store/table/table.selectors.ts
+++ src/app/store/table/table.selectors.ts
@@ -4,13 +4,24 @@ import { EReducerKeys } from '@shared/enum/app.enum';
 import { IAppState } from '@shared/interface/app.interface';
 import { ITableState } from '@shared/interface/table.interface';
 
-const selectTableDataFeature = createFeatureSelector<IAppState, ITableState>(EReducerKeys.table);
+const selectTableDataFeature = createFeatureSelector<IAppState, ITableState>(
+  EReducerKeys.table
+);
 
-export const selectTableData = createSelector(selectTableDataFeature, (state: ITableState) => state.content);
+export const selectTableData = createSelector(
+  selectTableDataFeature,
+  (state: ITableState) => state.content
+);
 
-export const selectSelectionState = createSelector(selectTableDataFeature, (state: ITableState) => state.hasSelection);
+export const selectSelectionState = createSelector(
+  selectTableDataFeature,
+  (state: ITableState) => state.hasSelection
+);
 
-export const selectAllRowsCount = createSelector(selectTableDataFeature, (state: ITableState) => state.allRowsCount);
+export const selectAllRowsCount = createSelector(
+  selectTableDataFeature,
+  (state: ITableState) => state.allRowsCount
+);
 
 export const selectSelectedRowsCount = createSelector(
   selectTableDataFeature,
---
+++ src/app/store/table/test/table.selectors.spec.ts
@@ -0,0 +1,87 @@
+import { ITableState } from '@shared/interface/table.interface';
+import {
+  selectAllRowsCount,
+  selectIsAllRowsSelected,
+  selectSelectedRowsCount,
+  selectSelectionState,
+  selectTableData,
+} from '../table.selectors';
+
+describe('TableSelectors', () => {
+  let initialState: ITableState;
+  let contentSize: number;
+  let secondTitle: string;
+
+  beforeEach(() => {
+    const allRowsCount = 101;
+    const selectedRowsCount = 38;
+    const hasSelection = true;
+
+    initialState = {
+      content: [
+        {
+          thumbnail: {
+            url: 'url1',
+            width: 101,
+            height: 101,
+          },
+          publishedAt: new Date(),
+          title: 'title1',
+          description: 'description1',
+          videoId: 'videoId1',
+        },
+        {
+          thumbnail: {
+            url: 'url2',
+            width: 202,
+            height: 202,
+          },
+          publishedAt: new Date(),
+          title: 'title2',
+          description: 'description2',
+          videoId: 'videoId2',
+        },
+      ],
+      hasSelection,
+      allRowsCount,
+      selectedRowsCount,
+    };
+
+    contentSize = initialState.content?.length;
+    secondTitle = initialState.content[1]?.title;
+  });
+
+  it('should select table data', () => {
+    const result = selectTableData.projector(initialState);
+
+    expect(result.length).toEqual(contentSize);
+    expect(result).toEqual(initialState.content);
+    expect(result[1].title).toEqual(secondTitle);
+  });
+
+  it('should select state of selection', () => {
+    const result = selectSelectionState.projector(initialState);
+
+    expect(result).toBeTrue();
+  });
+
+  it('should select all rows count', () => {
+    const result = selectAllRowsCount.projector(initialState);
+
+    expect(result).toEqual(initialState.allRowsCount);
+  });
+
+  it('should get selected rows count', () => {
+    const result = selectSelectedRowsCount.projector(initialState);
+
+    expect(result).toEqual(initialState.selectedRowsCount);
+  });
+
+  it('should select comparison of all and selected rows', () => {
+    const result = selectIsAllRowsSelected.projector(initialState);
+    const isAllSelected =
+      initialState.allRowsCount === initialState.selectedRowsCount;
+
+    expect(result).toEqual(isAllSelected);
+  });
+});
