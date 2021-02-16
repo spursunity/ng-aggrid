17d06a6fc0c2b6479c2c59d0820849b9b54dc337
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-02-16 10:36:28 +0300

    refactor: video title renderer tests

--- src/app/table/video-title-renderer/video-title-renderer.component.spec.ts
+++ src/app/table/video-title-renderer/video-title-renderer.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ICellRendererParams } from 'ag-grid-community';
 
 import { VideoTitleRendererComponent } from './video-title-renderer.component';
 
 describe('VideoTitleRendererComponent', () => {
   let component: VideoTitleRendererComponent;
   let fixture: ComponentFixture<VideoTitleRendererComponent>;
+  const params = {
+    data: {
+      title: 'mock.title',
+      videoLink: 'mock.videoLink',
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,5 +27,25 @@ describe('VideoTitleRendererComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have initial prop "videoLink" as empty string', () => {
+    expect(component.videoLink).toEqual('');
+  });
+
+  it('should have initial prop "videoTitle" as empty string', () => {
+    expect(component.videoTitle).toEqual('');
+  });
+
+  it('should change prop "videoLink" after "agInit"', () => {
+    component.agInit(params as ICellRendererParams);
+
+    expect(component.videoLink).toEqual(params.data.videoLink);
+  });
+
+  it('should change prop "videoTitle" after "agInit"', () => {
+    component.agInit(params as ICellRendererParams);
+
+    expect(component.videoTitle).toEqual(params.data.title);
   });
 });
